a9d267144738cf2cbcea18b2e3db77e01d068d43
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2022-03-16 08:38:33 +0200

    Add files via upload

---
+++ Projects1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ok = 0;
+            Console.Write("Nr. produse:");
+            int nrProduse =
+            int.Parse(Console.ReadLine());
+            // array de produse
+            Produs[] produse = new Produs[100];
+            for (int cnt = 0; cnt < nrProduse; cnt++)
+            {
+                // instantierea unui Produs
+                Produs prod = new Produs();
+                prod = prod.createProdus();
+               /* for(int i = 0; i < produse.Length; i++)
+                {
+                    if (prod.CodIntern == produse[i].CodIntern)
+                    {
+                        ok++;
+                    }
+                }*/
+                if (ok == 0)
+                {
+                    produse[cnt] = prod;
+                }
+                ok = 0;
+               
+               
+            }
+            // afisam produsele
+            Console.WriteLine("Produsele sunt:");
+            Produs p = new Produs();
+            p.afisProdus(produse);
+            Console.Write("Nr. servicii:");
+            int nrServicii =int.Parse(Console.ReadLine());
+            Serviciu[] servicii = new Serviciu[100];
+            for (int cnt = 0; cnt < nrProduse; cnt++)
+            {
+                // instantierea unui Produs
+                Serviciu serv = new Serviciu();
+                serv = serv.createServiciu();
+                /* for (int i = 0; i < servicii.Length; i++)
+                 {
+                     if (serv.CodIntern == servicii[i].CodIntern)
+                     {
+                         ok++;
+                     }
+                 }
+                 if (ok == 0)
+                 {
+                     servicii[cnt] = serv;
+                 }
+                 ok = 0;*/
+                Console.WriteLine("Serviciile sunt:");
+                Serviciu s= new Serviciu();
+                s.afisServiciu(servicii);
+
+            }
+        }
+    }
+    }
+
---
+++ Projects1/Serviciu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2
+{
+    class Serviciu
+    {
+
+        private String codIntern;// codul Intern
+        private long id;// identificator
+        private String nume;// numele serviciului
+
+        public string CodIntern { get => codIntern; set => codIntern = value; }
+        public long Id { get => id; set => id = value; }
+        public string Nume { get => nume; set => nume = value; }
+        public Serviciu createServiciu()
+        {
+            Serviciu serviciu = new Serviciu();
+            Console.WriteLine("Introdu un serviciu");
+            Console.Write("Numele:");
+            serviciu.Nume = Console.ReadLine();
+            Console.Write("Codul intern:");
+            serviciu.CodIntern = Console.ReadLine();
+           
+            return serviciu;
+        }
+        public void afisServiciu(Serviciu[] serviciu)
+        {
+            for (int cnt = 0; cnt < serviciu.Length; cnt++)
+            {
+                Serviciu serv = serviciu[cnt];
+                Console.WriteLine("Produs: " +
+                serv.Nume + "[" +
+                serv.CodIntern +
+                "] " );
+            }
+        }
+    }
+}
