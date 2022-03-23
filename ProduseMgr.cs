7a43b174c0477776b18b5a75f911d8b1ea6df77c
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2022-03-23 11:46:23 +0200

    Add files via upload

---
+++ ProduseMgr.cs
@@ -0,0 +1,57 @@
+﻿using L3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app1
+{
+    class ProduseMgr
+    {
+        private Produs[] produse;
+        private int nrProduse ;
+
+        public ProduseMgr()
+        {
+            this.produse = new Produs[100];
+            this.nrProduse = 0;
+        }
+        public ProduseMgr(Produs[] produse, int nrProduse)
+        {
+            this.produse = produse;
+            this.nrProduse = nrProduse;
+        }
+
+      
+        public void ReadProduse( int nrProduse)
+        {
+            
+            for(int i = 0; i < nrProduse; i++)
+            {
+                Console.WriteLine("Introdu un produs");
+                Console.Write("Numele:");
+                String nume = Console.ReadLine();
+                Console.Write("Codul intern:");
+                String CodIntern = Console.ReadLine();
+                Console.Write("Producator :");
+                String producator = Console.ReadLine();
+                produse[i] = new Produs(i, nume, CodIntern, producator);
+            }
+           
+       
+        }
+       public void WriteProduse()
+        {
+            for (int i = 0; i < nrProduse; i++)
+            {
+                //Console.WriteLine("Id "+produse[i].Id1+" Nume: "+produse[i].Nume1+" Cod Intern: "+produse[i].CodIntern1+" Producator: "+produse[i].Producator1);
+                produse[i].AltaDescriere();
+            }
+          
+
+        }
+
+    }
+     
+    }
---
+++ ServiciuMgr.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using L3;
+
+namespace app1
+{
+    class ServiciuMgr
+    {
+        private Serviciu[] servicii;
+        private int nrServicii;
+
+        public ServiciuMgr()
+        {
+            this.servicii = new Serviciu[100];
+            this.nrServicii = 0;
+        }
+        public ServiciuMgr(Serviciu[] servicii, int nrServicii)
+        {
+            this.servicii = servicii;
+            this.nrServicii = nrServicii;
+        }
+        public void  ReadServicii(int nrServicii)
+        {
+
+            for (int i = 0; i < nrServicii; i++)
+            {
+                Console.WriteLine("Introdu un serviciu");
+                Console.Write("Numele:");
+                String nume = Console.ReadLine();
+                Console.Write("Codul intern:");
+                String CodIntern = Console.ReadLine();
+                servicii[i] = new Serviciu(i, nume, CodIntern);
+            }
+            
+         
+
+        }
+        public void WriteServicii()
+        {
+            for (int i = 0; i < nrServicii; i++)
+            {
+                servicii[i].Descriere();
+                    
+               //Console.WriteLine("Id " + servicii[i].Id1 + "Nume: " + servicii[i].Nume1 + " Cod Intern: " + servicii[i].CodIntern1);
+
+            }
+
+
+        }
+    }
+}
