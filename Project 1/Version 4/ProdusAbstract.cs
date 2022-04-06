9e051784f98aa7fc54bf768ebc572ea7ec087c79
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2022-04-06 11:45:23 +0300

    Add files via upload

---
+++ Project 1/Version 4/ProdusAbstract.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class ProdusAbstract
+    {
+        private long Id;
+        private String Nume;
+        private String CodIntern;
+        private double Pret;
+        private String Categorie;
+
+        public ProdusAbstract(long id, string nume, string codIntern, double pret, string categorie)
+        {
+            Id = id;
+            Nume = nume;
+            CodIntern = codIntern;
+            Pret = pret;
+            Categorie = categorie;
+        }
+
+        public long Id1 { get => Id; set => Id = value; }
+        public string Nume1 { get => Nume; set => Nume = value; }
+        public string CodIntern1 { get => CodIntern; set => CodIntern = value; }
+        public double Pret1 { get => Pret; set => Pret = value; }
+        public string Categorie1 { get => Categorie; set => Categorie = value; }
+
+     
+        public abstract String Descriere();
+        public virtual String AltaDescriere()
+        {
+            return "Id [" + Id1 + "]: " + Nume1 + " cod intern :[" + CodIntern1 + "] "+" Pret:"+Pret1+" Categorie:"+Categorie1;
+        }
+    }
+}
---
+++ Project 1/Version 4/ProdusAbstractMgr.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    abstract class ProduseAbstractMgr
+    {
+        //private static ProdusAbstract[] elemente = new ProdusAbstract[100];
+        //private static int nrElemente = 0;
+        protected static List<ProdusAbstract> elemente=new List<ProdusAbstract>();
+
+        //public static ProdusAbstract[] Elemente { get => elemente; set => elemente = value; }
+       // public static int NrElemente { get => nrElemente; set => nrElemente = value; }
+
+        public void Write2Console()
+        {  
+           foreach(ProdusAbstract prod in elemente)
+            {
+                Console.WriteLine(prod.Descriere());
+            }
+        }
+        public void InitListafromXML()
+        {
+            //initializare lista dintr-un fisier XML
+            XmlDocument doc = new XmlDocument();
+            XmlDocument doc2 = new XmlDocument();
+            //incarca fisierul
+            doc.Load("C:\\Users\\cti20c117\\source\\repos\\ConsoleApp1\\Produse.xml"); //calea spre fisier
+            doc2.Load("C:\\Users\\cti20c117\\source\\repos\\ConsoleApp1\\Servicii.xml");
+                                        //selecteaza nodurile
+            XmlNodeList lista_noduri = doc.SelectNodes("/produse/Produs");
+            XmlNodeList lista_noduri2 = doc.SelectNodes("/servicii/Serviciu");
+            foreach (XmlNode nod in lista_noduri)
+            {
+                //itereaza si selecteaza simpurile fiecarui nod si 
+                //informatia continuta in cadrul proprietatii InnerText
+                string nume = nod["Nume"].InnerText;
+                string codIntern = nod["CodIntern"].InnerText;
+                string producator = nod["Producator"].InnerText;
+                int pret = int.Parse(nod["Pret"].InnerText);
+                string categorie = nod["Categorie"].InnerText;
+
+                //adauga in lista produse
+                elemente.Add(new Produs
+                (elemente.Count + 1, nume, codIntern,producator, pret, categorie));
+            }
+            foreach (XmlNode nod in lista_noduri2)
+            {
+                //itereaza si selecteaza simpurile fiecarui nod si 
+                //informatia continuta in cadrul proprietatii InnerText
+                string nume = nod["Nume"].InnerText;
+                string codIntern = nod["CodIntern"].InnerText;
+                int pret = int.Parse(nod["Pret"].InnerText);
+                string categorie = nod["Categorie"].InnerText;
+
+                //adauga in lista produse
+                elemente.Add(new Serviciu
+                (elemente.Count + 1, nume, codIntern, pret, categorie));
+            }
+        }
+    }
+}
