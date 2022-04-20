9c96d5702eb20b7845701b7653df7dcf03081f23
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2022-04-20 08:51:59 +0300

    Add files via upload

---
+++ Project 1/Version 6/incomplete/Pachet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Pachet : ProdusAbstract,IPackageble
+    {
+        //List<ProdusAbstract> elem_pachet=new List<ProdusAbstract>();
+
+        private List<IPackageble> elem_pachet = new List<IPackageble>();
+        public Pachet(List<IPackageble>elem_pachet, long id, string nume, string codIntern, double pret, string categorie) : base(id, nume, codIntern, pret, categorie)
+        {
+            this.elem_pachet = elem_pachet; 
+           
+           
+        }
+
+      
+       public void AddElement(IPackageble elem)
+        {
+            if (elem.canAddToPackage(this))
+            {
+                elem_pachet.Add(elem);
+            }
+            
+        }
+        public bool canAddToPackage(Pachet pachet)
+        {
+            return false;
+        }
+
+        public override string Descriere()
+        {
+            return "Id [" + Id1 + "]: " + Nume1 + " cod intern :[" + CodIntern1 + "] " + " Pret:" + Pret1 + " Categorie:" + Categorie1;
+        }
+    }
+}
---
+++ Project 1/Version 6/incomplete/PacheteMgr.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class PacheteMgr : ProduseAbstractMgr
+    {
+        
+        public void ReadPachet()
+        {
+            Pachet pachet = new Pachet();
+            ProduseMgr produseMgr = new ProduseMgr();
+            foreach(ProduseAbstractMgr produse in produseMgr)
+            {
+                pachet.AddElement(produse);
+            }
+         
+        }
+    }
+}
